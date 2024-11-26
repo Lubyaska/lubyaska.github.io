1d6d41e49e05bf4f91e9ad2abc9c2f0c7a197542
Lubyaska <117581573+Lubyaska@users.noreply.github.com>
2024-11-27 01:46:58 +0700

    contact.php

--- mail/contact.php
+++ mail/contact.php
@@ -12,8 +12,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "maria.lubyaska@gmail.com";
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
+$header .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
